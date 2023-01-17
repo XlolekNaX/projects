3c33ed32d5e0a5ec58f15fec33bea0c2b8fbe6ee
XlolekNaX <91343698+XlolekNaX@users.noreply.github.com>
2023-01-17 16:30:06 +0100

    Add "Text typing" directory

---
+++ Text typing/script.js
@@ -0,0 +1,7 @@
+// kod do literowania tekstu (typing)
+var typed = new Typed(".typing", {
+    strings: ["YouTuberem", "Developerem", "Designerem"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
